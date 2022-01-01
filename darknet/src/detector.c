70eca93b0874b9f469904dc0289255a65c4902a6
Mario Carmona Segovia <44337691+Mario-Carmona@users.noreply.github.com>
2022-01-01 17:58:10 +0100

    Añadido calculo recall train

--- darknet/src/detector.c
+++ darknet/src/detector.c
@@ -910,6 +910,82 @@ void validate_detector_recall(char *datacfg, char *cfgfile, char *weightfile)
     }
 }
 
+void train_detector_recall(char *datacfg, char *cfgfile, char *weightfile)
+{
+    network net = parse_network_cfg(cfgfile);
+    if (weightfile) {
+        load_weights(&net, weightfile);
+    }
+    //set_batch_network(&net, 1);
+    fuse_conv_batchnorm(net);
+    srand(time(0));
+
+    //list *plist = get_paths("data/coco_val_5k.list");
+    list *options = read_data_cfg(datacfg);
+    char *train_images = option_find_str(options, "train", "data/train.txt");
+    list *plist = get_paths(train_images);
+    char **paths = (char **)list_to_array(plist);
+
+    //layer l = net.layers[net.n - 1];
+
+    int j, k;
+
+    int m = plist->size;
+    int i = 0;
+
+    float thresh = .001;
+    float iou_thresh = .5;
+    float nms = .4;
+
+    int total = 0;
+    int correct = 0;
+    int proposals = 0;
+    float avg_iou = 0;
+
+    for (i = 0; i < m; ++i) {
+        char *path = paths[i];
+        image orig = load_image(path, 0, 0, net.c);
+        image sized = resize_image(orig, net.w, net.h);
+        char *id = basecfg(path);
+        network_predict(net, sized.data);
+        int nboxes = 0;
+        int letterbox = 0;
+        detection *dets = get_network_boxes(&net, sized.w, sized.h, thresh, .5, 0, 1, &nboxes, letterbox);
+        if (nms) do_nms_obj(dets, nboxes, 1, nms);
+
+        char labelpath[4096];
+        replace_image_to_label(path, labelpath);
+
+        int num_labels = 0;
+        box_label *truth = read_boxes(labelpath, &num_labels);
+        for (k = 0; k < nboxes; ++k) {
+            if (dets[k].objectness > thresh) {
+                ++proposals;
+            }
+        }
+        for (j = 0; j < num_labels; ++j) {
+            ++total;
+            box t = { truth[j].x, truth[j].y, truth[j].w, truth[j].h };
+            float best_iou = 0;
+            for (k = 0; k < nboxes; ++k) {
+                float iou = box_iou(dets[k].bbox, t);
+                if (dets[k].objectness > thresh && iou > best_iou) {
+                    best_iou = iou;
+                }
+            }
+            avg_iou += best_iou;
+            if (best_iou > iou_thresh) {
+                ++correct;
+            }
+        }
+        //fprintf(stderr, " %s - %s - ", paths[i], labelpath);
+        fprintf(stderr, "%5d %5d %5d\tRPs/Img: %.2f\tIOU: %.2f%%\tRecall:%.2f%%\n", i, correct, total, (float)proposals / (i + 1), avg_iou * 100 / total, 100.*correct / total);
+        free(id);
+        free_image(orig);
+        free_image(sized);
+    }
+}
+
 typedef struct {
     box b;
     float p;
@@ -2071,6 +2147,7 @@ void run_detector(int argc, char **argv)
     else if (0 == strcmp(argv[2], "train")) train_detector(datacfg, cfg, weights, gpus, ngpus, clear, dont_show, calc_map, thresh, iou_thresh, mjpeg_port, show_imgs, benchmark_layers, chart_path);
     else if (0 == strcmp(argv[2], "valid")) validate_detector(datacfg, cfg, weights, outfile);
     else if (0 == strcmp(argv[2], "recall")) validate_detector_recall(datacfg, cfg, weights);
+    else if (0 == strcmp(argv[2], "recall_train")) train_detector_recall(datacfg, cfg, weights);
     else if (0 == strcmp(argv[2], "map")) validate_detector_map(datacfg, cfg, weights, thresh, iou_thresh, map_points, letter_box, NULL);
     else if (0 == strcmp(argv[2], "calc_anchors")) calc_anchors(datacfg, num_of_clusters, width, height, show);
     else if (0 == strcmp(argv[2], "draw")) {
